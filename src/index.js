f4e3e67f22aa2e0515ed48b217f7a1774b107e06
davorbadrov <dev.davor.badrov@gmail.com>
2017-08-15 11:40:32 +0200

    remove console.log

--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ const Hapi = require('hapi');
 const config = require('./lib/config');
 const server = new Hapi.Server();
 
-console.log('config', JSON.stringify(config, null, 2));
-
 server.connection({ port: config.appPort });
 server.start(err => {
     if (err) {
