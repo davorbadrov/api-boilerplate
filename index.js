e4c5d2f0a389b4ab6e8f073234fea0fcb738ad20
davorbadrov <dev.davor.badrov@gmail.com>
2017-08-16 13:00:59 +0200

    formatted js files

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const dotenv = require('dotenv');
+const dotenv = require('dotenv')
 
 if (process.env.NODE_ENV !== 'production') {
-    const settings = dotenv.load();
+  const settings = dotenv.load()
 }
 
-require('./src');
+require('./src')
--- src/lib/config.test.js
+++ src/lib/config.test.js
@@ -1,35 +1,35 @@
-const getConfig = require('./config');
+const getConfig = require('./config')
 
 test('config is properly processed', () => {
-    const config = getConfig({
-        APP_PORT: '8080',
-        DB_HOST: 'database_host',
-        DB_NAME: 'database_name',
-        DB_PORT: '5432',
-        DB_USER: 'database_username',
-        DB_PASSWORD: 'database_password'
-    });
+  const config = getConfig({
+    APP_PORT: '8080',
+    DB_HOST: 'database_host',
+    DB_NAME: 'database_name',
+    DB_PORT: '5432',
+    DB_USER: 'database_username',
+    DB_PASSWORD: 'database_password'
+  })
 
-    const expected = {
-        appPort: 8080,
-        dbHost: 'database_host',
-        dbName: 'database_name',
-        dbPort: 5432,
-        dbUser: 'database_username',
-        dbPassword: 'database_password'
-    };
+  const expected = {
+    appPort: 8080,
+    dbHost: 'database_host',
+    dbName: 'database_name',
+    dbPort: 5432,
+    dbUser: 'database_username',
+    dbPassword: 'database_password'
+  }
 
-    expect(config).toEqual(expected);
-});
+  expect(config).toEqual(expected)
+})
 
 test(`config throws error if something's missing`, () => {
-    expect(() => {
-        getConfig({
-            DB_HOST: 'database_host',
-            DB_NAME: 'database_name',
-            DB_PORT: 'database_port',
-            DB_USER: 'database_username',
-            DB_PASSWORD: 'database_password'
-        });
-    }).toThrow();
-});
+  expect(() => {
+    getConfig({
+      DB_HOST: 'database_host',
+      DB_NAME: 'database_name',
+      DB_PORT: 'database_port',
+      DB_USER: 'database_username',
+      DB_PASSWORD: 'database_password'
+    })
+  }).toThrow()
+})
