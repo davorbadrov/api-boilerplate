69447d55898be8777af9002ef776c3b9066a2431
davorbadrov <dev.davor.badrov@gmail.com>
2017-08-16 12:58:53 +0200

    add node env as required

--- src/lib/config.js
+++ src/lib/config.js
@@ -1,5 +1,5 @@
-const joi = require('joi');
-const { pick, mapKeys, camelCase } = require('lodash');
+const joi = require('joi')
+const { pick, mapKeys, camelCase } = require('lodash')
 
 /**
  * Returns object with only validated env variables in camel casing
@@ -7,37 +7,41 @@ const { pick, mapKeys, camelCase } = require('lodash');
  * @param  {object} enviromentVariables  takes process.env
  * @return {object}                      object with camel cased validated params
  */
-module.exports = function getConfig(enviromentVariables) {
-    const config = validateEnvVariables(enviromentVariables);
-    const camelCasedEnv = convertEnvKeysToCamelCase(config);
-    return camelCasedEnv;
+module.exports = function getConfig (enviromentVariables) {
+  const config = validateEnvVariables(enviromentVariables)
+  const camelCasedEnv = convertEnvKeysToCamelCase(config)
+  return camelCasedEnv
 }
 
-function validateEnvVariables(enviromentVariables) {
-    const configSchemaParams = {
-        APP_PORT: joi.number().required(),
-        DB_HOST: joi.string().required(),
-        DB_NAME: joi.string().required(),
-        DB_PORT: joi.number().required(),
-        DB_USER: joi.string().required(),
-        DB_PASSWORD: joi.string().allow('').required()
-    };
+function validateEnvVariables (enviromentVariables) {
+  const configSchemaParams = {
+    NODE_ENV: joi
+      .string()
+      .allow(['development', 'production', 'test'])
+      .default('development'),
+    APP_PORT: joi.number().required(),
+    DB_HOST: joi.string().required(),
+    DB_NAME: joi.string().required(),
+    DB_PORT: joi.number().required(),
+    DB_USER: joi.string().required(),
+    DB_PASSWORD: joi.string().allow('').required()
+  }
 
-    const envVars = pick(enviromentVariables, Object.keys(configSchemaParams));
-    const envVarsSchema = joi.object().keys(configSchemaParams).required();
+  const envVars = pick(enviromentVariables, Object.keys(configSchemaParams))
+  const envVarsSchema = joi.object().keys(configSchemaParams).required()
 
-    const { error, value: validatedEnvironmentVariables } = joi.validate(
-        envVars,
-        envVarsSchema
-    );
+  const { error, value: validatedEnvironmentVariables } = joi.validate(
+    envVars,
+    envVarsSchema
+  )
 
-    if (error) {
-        throw new Error(`Config validation error: ${error.message}`);
-    }
+  if (error) {
+    throw new Error(`Config validation error: ${error.message}`)
+  }
 
-    return validatedEnvironmentVariables;
+  return validatedEnvironmentVariables
 }
 
-function convertEnvKeysToCamelCase(object) {
-    return mapKeys(object, (_, key) => camelCase(key));
+function convertEnvKeysToCamelCase (object) {
+  return mapKeys(object, (_, key) => camelCase(key))
 }
