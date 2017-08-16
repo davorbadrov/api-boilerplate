8b6959a87e6324cf7f16123abfde7cf9aeaa2987
davorbadrov <dev.davor.badrov@gmail.com>
2017-08-16 12:59:34 +0200

    changed whitespace

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
-const Hapi = require('hapi');
-const getConfig = require('./lib/config');
-const config = getConfig(process.env);
-const server = new Hapi.Server();
+const Hapi = require('hapi')
+const getConfig = require('./lib/config')
+const config = getConfig(process.env)
+const server = new Hapi.Server()
 
-server.connection({ port: config.appPort });
+server.connection({ port: config.appPort })
 server.start(err => {
-    if (err) {
-        throw err;
-    }
+  if (err) {
+    throw err
+  }
 
-    console.log(`Server running at: ${server.info.uri}`);
-});
+  console.log(`Server running at: ${server.info.uri}`)
+})
