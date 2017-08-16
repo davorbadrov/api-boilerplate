8054a9f2c85b61f435af575b78c2857bc1ae4768
davorbadrov <dev.davor.badrov@gmail.com>
2017-08-16 16:29:40 +0200

    add basic error reporting if server can't start

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 const server = require('./server')
 
-server.initialize().then(serverInstance => {
-  console.log(`Server running at: ${serverInstance.info.uri}`)
-})
+server
+  .initialize()
+  .then(serverInstance => {
+    console.log(`Server running at: ${serverInstance.info.uri}`)
+  })
+  .catch(err => {
+    console.error("Couldn't start server", err)
+    process.exit(1)
+  })
